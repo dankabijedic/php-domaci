80dcaf36042d4c88b8f47af71be5badeaedff036
dankabijedic <dankabijedic@gmail.com>
2022-05-22 23:28:57 +0100

    Osposobljeno je cuvanje novog aranzmana u bazi

--- forma/forma.php
+++ forma/forma.php
@@ -2,6 +2,8 @@
 
 include "../konekcija.php";
 include "../drzave.php";
+include "../aranzmani/aranzmaniobrada.php";
+
 $drzava = Drzava::getCountry($conn);
 
 
@@ -25,24 +27,43 @@ http://www.tooplate.com/view/2095-level
 
 -->
     <!-- load stylesheets -->
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"> <!-- Google web font "Open Sans" -->
-   <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css"> <!-- Font Awesome -->
     <link rel="stylesheet" href="css/bootstrap.min.css"> <!-- Bootstrap style -->
-    <link rel="stylesheet" type="text/css" href="slick/slick.css" />
-    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css" />
-    <link rel="stylesheet" type="text/css" href="css/datepicker.css" />
     <link rel="stylesheet" href="css/tooplate-style.css"> <!-- Templatemo style -->
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-          <![endif]-->
+
 </head>
 
 <body>
 
+<?php 
+    
+    if (isset($_POST['insertdata'])) {
+                    
+
+               $mesto = $_POST['mesto'];
+               $datum_polaska = $_POST['datum_polaska'];
+               $datum_povratka = $_POST['datum_povratka'];
+               $cena_u_evrima = $_POST['cena_u_evrima'];
+               $nacin_prevoza = $_POST['nacin_prevoza'];
+               $tip_smestaja = $_POST['tip_smestaja'];
+               $naziv_drzave = $_POST['naziv_drzave'];
+
+
+                $data = Array (
+                    "mesto" => $mesto,
+                    "datum_polaska" => $datum_polaska,					
+                    "datum_povratka" => $datum_povratka,					
+                    "cena_u_evrima" => $cena_u_evrima,    
+                    "nacin_prevoza" => $nacin_prevoza,    
+                    "tip_smestaja" => $tip_smestaja, 
+                    "id_drzave" => $naziv_drzave   
+            );	
+
+            $aranzman = new Aranzman(null, $mesto, $datum_polaska, $datum_povratka,$cena_u_evrima, $nacin_prevoza, $tip_smestaja, $naziv_drzave);
+            $aranzman->insert($conn, $data);
+
+        }
+    ?>
 
     <div class="tm-main-content" id="top">
         <div class="tm-top-bar-bg"></div>
@@ -52,17 +73,17 @@ http://www.tooplate.com/view/2095-level
                 <div class="row">
 
                     <nav class="navbar navbar-expand-md navbar-dark navbar-custom fixed-top">
-                        <a class="navbar-brand logo-text page-scroll" href="index.html">Magnus</a>
+                        <a class="navbar-brand logo-text page-scroll" href="../index.php">Magnus</a>
                         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                             <ul class="navbar-nav ml-auto">
                                 <li class="nav-item">
-                                    <a class="btn-round-custom" href="#">Pocetna</a>
+                                    <a class="btn-round-custom" href="../index.php">Pocetna</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="btn-round-custom" href="aranzmani/aranzmani.php">Aranzmani</a>
+                                    <a class="btn-round-custom" href="../aranzmani/aranzmani.php">Aranzmani</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="btn-round-custom" href="#services">Pretraga</a>
+                                    <a class="btn-round-custom" href="#">Pretraga</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="btn-round-custom" href="#callMe">Kontakt</a>
@@ -79,22 +100,22 @@ http://www.tooplate.com/view/2095-level
                 <div class="container ie-h-align-center-fix">
                     <div class="row">
                         <div class="col-xs-12 ml-auto mr-auto ie-container-width-fix">
-                            <form action="index.html" method="get" class="tm-search-form tm-section-pad-2">
+                            <form action="forma.php" method="post" class="tm-search-form tm-section-pad-2">
                                 <div class="form-row tm-search-form-row">
                                     <div class="form-group tm-form-element tm-form-element-100">
                                         <i class="fa fa-map-marker fa-2x tm-form-element-icon"></i>
                                         <select class="form-control" name="naziv_drzave" id="naziv_drzave" placeholder="Drzava">                                      
                                         <option value="" disabled selected>Drzava</option>
                                         <?php foreach($drzava as $d): ?>
-                        <option value="<?php echo $d->id_drzave;?>">
-                            <?php echo $d->naziv_drzave;?>
-                        </option>
-                        <?php endforeach; ?>
+                                           <option value="<?php echo $d->id_drzave;?>">
+                                          <?php echo $d->naziv_drzave;?>
+                                       </option>
+                                        <?php endforeach; ?>
                                         </select>
                                     </div>
                                     <div class="form-group tm-form-element tm-form-element-100">
                                         <i class="fa fa-map-marker fa-2x tm-form-element-icon"></i>
-                                        <input name="Mesto" type="text" class="form-control" id="mesto" placeholder="Mesto">
+                                        <input name="mesto" type="text" class="form-control" id="mesto" placeholder="Mesto">
                                     </div>
                                     <div class="form-group tm-form-element tm-form-element-50">
                                         <i class="fa fa-calendar fa-2x tm-form-element-icon"></i>
@@ -111,27 +132,27 @@ http://www.tooplate.com/view/2095-level
                                         <input name="cena_u_evrima" type="text" class="form-control" id="cena_u_evrima" placeholder="Cena u evrima">
                                     </div>
                                     <div class="form-group tm-form-element tm-form-element-2">
-                                        <select name="adult" class="form-control tm-select" id="nacin_prevoza">
+                                        <select name="nacin_prevoza" class="form-control tm-select" id="nacin_prevoza">
                                             <option value="">Nacin prevoza</option>
-                                            <option value="1">Autobus</option>
-                                            <option value="2">Avion</option>
+                                            <option value="Autobus">Autobus</option>
+                                            <option value="Avion">Avion</option>
                                         </select>
                                         <i class="fa fa-2x fa-user tm-form-element-icon"></i>
                                     </div>
                                     <div class="form-group tm-form-element tm-form-element-2">
-                                        <select name="children" class="form-control tm-select" id="tip_smestaja">
+                                        <select name="tip_smestaja" class="form-control tm-select" id="tip_smestaja">
                                             <option value="">Tip smestaja</option>
-                                            <option value="1">Hotel *</option>
-                                            <option value="2">Hotel **</option>
-                                            <option value="3">Hotel ***</option>
-                                            <option value="4">Hotel ****</option>
-                                            <option value="5">Hotel *****</option>
+                                            <option value="Hotel *">Hotel *</option>
+                                            <option value="Hotel **">Hotel **</option>
+                                            <option value="Hotel ***">Hotel ***</option>
+                                            <option value="Hotel ****">Hotel ****</option>
+                                            <option value="Hotel *****">Hotel *****</option>
 
                                         </select>
                                         <i class="fa fa-user tm-form-element-icon tm-form-element-icon-small"></i>
                                     </div>
                                     <div class="form-group tm-form-element tm-form-element-2">
-                                        <button type="submit" class="btn btn-primary tm-btn-search">Sacuvaj</button>
+                                        <button type="submit" name="insertdata" class="btn btn-primary tm-btn-search">Sacuvaj</button>
                                     </div>
                                 </div>
 
@@ -153,8 +174,6 @@ http://www.tooplate.com/view/2095-level
                 </div>
             </div>
         </div>
-
-
 </body>
 
 </html>
