3b94db26b2a977f4e2d1ad5c08bc53fd4004f00c
dankabijedic <dankabijedic@gmail.com>
2022-05-22 23:28:24 +0100

    Popravljena je insert funkcija

--- aranzmani/aranzmaniobrada.php
+++ aranzmani/aranzmaniobrada.php
@@ -16,20 +16,14 @@
         }
 		
         public function insert($table, $data){
-			$query = "INSERT INTO $table (id_aranzmana,mesto,datum_polaska,datum_povratka,cena_u_evrima,nacin_prevoza,tip_smestaja,id_drzave) VALUES ('$data[id_aranzmana]','$data[mesto]','$data[datum_polaska]','$data[datum_povratka]','$data[cena_u_evrima]','$data[nacin_prevoza]','$data[tip_smestaja]','$data[id_drzave]')";
+			
 
-			if ($sql = $this->conn->query($query)) {
-				return true;
-			}else{
-				return false;
-            }
-
-			if($data['id_aranzmana'] === '' || $data['mesto'] === '' || $data['datum_polaska'] === ''|| $data['datum_povratka'] === '' || $data['cena_u_evrima'] === '' || $data['nacin_prevoza'] === '' || $data['tip_prevoza'] === '' || $data['id_drzave'] === ''){
+			if($data['mesto'] === '' || $data['datum_polaska'] === ''|| $data['datum_povratka'] === '' || $data['cena_u_evrima'] === '' || $data['nacin_prevoza'] === '' || $data['tip_smestaja'] === '' || $data['id_drzave'] === ''){
 				$this-> poruka ='Polja moraju biti popunjena';
 				
 				}else{
 				
-					$save=$data->query("INSERT INTO predstave(naslov,cena,trajanje,datum_izvodjenja,id_vrste) VALUES ('".$data['naslov']."','".$data['cena']."','".$data['trajanje']."','".$data['datum_izvodjenja']."','".$data['id_vrste']."')") or die($data->error);
+					$save=$table->query("INSERT INTO aranzmani(mesto,datum_polaska,datum_povratka,cena_u_evrima,nacin_prevoza,tip_smestaja,id_drzave) VALUES ('".$data['mesto']."','".$data['datum_polaska']."','".$data['datum_povratka']."','".$data['cena_u_evrima']."','".$data['nacin_prevoza']."','".$data['tip_smestaja']."','".$data['id_drzave']."')") or die($data->error);
 					if($save){
 						$this -> poruka = 'Uspesno sacuvana predstava';
 					}else{
