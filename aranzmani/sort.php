49d30eb8f66d6e85c2b124405f347554c16a25ec
dankabijedic <dankabijedic@gmail.com>
2022-06-06 20:03:51 +0100

    Osposobljeno sortiranje tabele

--- aranzmani/sort.php
+++ aranzmani/sort.php
@@ -1,6 +1,5 @@
 <?php  
-//include "../konekcija.php";
-include "aranzmani.php";
+
 
 $connect = mysqli_connect("localhost", "root", "", "turisticka_agencija");  
 
@@ -15,30 +14,29 @@ $connect = mysqli_connect("localhost", "root", "", "turisticka_agencija");
       $order = 'desc';  
  }  
  $query = "SELECT * , d.naziv_drzave FROM drzave d INNER JOIN aranzmani a on (d.id_drzave=a.id_drzave)
-           ORDER BY a.id_aranzmana DESC";  
- $result = mysqli_query($conn, $query);  
+           ORDER BY ".$_POST["column_name"]."  ".$_POST["order"]."";  
+ $result = mysqli_query($connect, $query);  
  $output .= '  
- <table class="table" id="aranzmaniTabela">
-     <thead>
-          <tr>
-               <th><a class="column_sort" id="id_aranzmana" data-order="'.$order.'" href="#">ID</a></th>
-               <th><a class="column_sort" id="mesto" data-order="'.$order.'" href="#">Mesto</a></th>
-               <th><a class="column_sort" id="datum_polaska" data-order="'.$order.'" href="#">Datum polaska</a></th>
-               <th><a class="column_sort" id="datum_povratka" data-order="'.$order.'" href="#">Datum povratka</a></th>
-               <th><a class="column_sort" id="cena_u_evrima" data-order="'.$order.'" href="#">Cena u evrima</a></th> 
-               <th><a class="column_sort" id="nacin_prevoza" data-order="'.$order.'" href="#">Nacin prevoza</a></th> 
-               <th><a class="column_sort" id="tip_smestaja" data-order="'.$order.'" href="#">Tip smestaja</a></th> 
+ <table class="table table-hover" id="aranzmaniTabela">
+ <thead>
+     <tr>
+         <th scope="col"><a class="column_sort" id="id_aranzmana" data-order="'.$order.'" href="#">ID</a></th>
+         <th scope="col"><a class="column_sort" id="mesto" data-order="'.$order.'" href="#">Mesto</a></th>
+         <th scope="col"><a class="column_sort" id="datum_polaska" data-order="'.$order.'" href="#">Datum polaska</a></th>
+         <th scope="col"><a class="column_sort" id="datum_povratka" data-order="'.$order.'" href="#">Datum povratka</a></th>
+         <th scope="col"><a class="column_sort" id="cena_u_evrima" data-order="'.$order.'" href="#">Cena u evrima</a></th> 
+         <th scope="col"><a class="column_sort" id="nacin_prevoza" data-order="'.$order.'" href="#">Nacin prevoza</a></th> 
+         <th scope="col"><a class="column_sort" id="tip_smestaja" data-order="'.$order.'" href="#">Tip prevoza</a></th> 
+         <th scope="col"><a class="column_sort" id="tip_smestaja" data-order="'.$order.'" href="#">Drzava</th>
 
-               <th>Drzava</th>
-               <th>Izmeni</th>
-               <th>Obrisi</th>
-          </tr>
-     </thead>
+         <th scope="col">Izmeni</th>
+         <th scope="col">Obrisi</th>
+     </tr>
  ';  
  while($row = mysqli_fetch_array($result))  
  {  
       $output .= '  
-     <tbody id="myTable">
+  
           <tr>
                         
                <td>' . $row['id_aranzmana'] . '</td>
@@ -49,18 +47,20 @@ $connect = mysqli_connect("localhost", "root", "", "turisticka_agencija");
                <td>' . $row['nacin_prevoza'] . '</td>
                <td>' . $row['tip_smestaja'] . '</td>
                <td>' . $row['naziv_drzave'] . '</td>
+               
+           
+               <td> 
+               <button type="edit" data-role="update" class="btn btn-success editbtn">IZMENI</button>
+           </td>
+           <td> 
+               <button type="button" class="btn btn-danger deletebtn">OBRISI</button>
+           </td>
 
-               <td> 
-                    <button type="edit" class="btn btn-success editbtn">IZMENI</button>
-               </td>
-               <td> 
-                    <button type="button" class="btn btn-danger deletebtn">OBRISI</button>
-               </td>
 
           </tr>
                         
                         
-     </tbody>
+
       ';  
  }
  
